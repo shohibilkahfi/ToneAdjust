24689cdaa2a06de798de19d6a7c93fe1528fd158
shohibilkahfi <shohibil@orenda.co.id>
2025-03-03 14:51:53 +0700

    Initial Commit

---
+++ ToneAdjust/ToneAdjustApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneAdjustApp.swift
+//  ToneAdjust
+//
+//  Created by Orenda M1 on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ToneAdjustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
